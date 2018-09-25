0fa5582b4a4a527e346a97caf2a09de92eba12fd
Kimura Kazuyuki <tecokimura@gmail.com>
2018-09-26 01:22:14 +0900

    Music controll and Mouse key test but failed

--- keyboards/hhkb/keymaps/tecokimura/keymap.c
+++ keyboards/hhkb/keymaps/tecokimura/keymap.c
@@ -11,9 +11,15 @@
 #define LT_SPC  LT(LY_SPC, KC_SPC)
 #define LT_TAB  LT(LY_TAB, KC_TAB) 
 #define LT_LFN  LT(LY_LFN, KC_TAB) 
-#define LT_RFN  LT(LY_LFN, KC_TAB) 
+#define LT_RFN  LT(LY_RFN, _______) 
 
 #define JP_PLUS S(KC_SCLN)
+#define JP_ASTR S(KC_QUOT)
+
+#define MOUSEKEY_DELAY       50 
+#define MOUSEKEY_INTERVAL    5 
+#define MOUSEKEY_MAX_SPEED   50
+#define MOUSEKEY_TIME_TO_MAX 100
 
 #define TD_LKKO TD(TD_LKAKKO) // (<[{(((...
 #define TD_RKKO TD(TD_RKAKKO) // (<[{(((...
@@ -150,21 +156,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LY_TAB] = LAYOUT_JP(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_JYEN, KC_BSPC,
         _______, KC_BTN1, KC_BTN2, KC_E,    KC_R,    KC_TAB,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_P,    KC_LBRC, KC_RBRC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
+        KC_LCTL, KC_VOLU, KC_MUTE, KC_VOLU, KC_F,    KC_G,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, KC_RO, KC_UP, KC_RSFT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
     [LY_LFN] = LAYOUT_JP(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_TAB,  _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_MINS, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    JP_PLUS, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, _______, _______,
+        _______, _______, KC_SPC,  KC_DEL,  _______, _______, _______, KC_4,    KC_5,    KC_6,    JP_PLUS, JP_ASTR, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_SLSH, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
         
     [LY_RFN] = LAYOUT_JP(
         KC_PWR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, KC_DEL,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,  _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, KC_PENT,
+        _______, KC_VOLU, KC_MUTE, KC_VOLD, _______, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, KC_PENT,
         _______, _______, _______, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)};
 
