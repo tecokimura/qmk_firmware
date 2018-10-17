acd37460773f3d5034c7126e1820b0fe5d34861e
Kimura Kazuyuki <tecokimura@gmail.com>
2018-10-18 02:46:27 +0900

    Rebalance JIS for Ergo42

--- keyboards/ergo42/keymaps/tecokimura/keymap.c
+++ keyboards/ergo42/keymaps/tecokimura/keymap.c
@@ -39,17 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [LY_NORMAL] = LAYOUT( \
-    LT_MOUS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC,    KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_MINS, \
-    KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    S(KC_8),          S(KC_9),    KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    S(KC_LBRC),       S(KC_RBRC), KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
-    LT_NUMB,  KC_LGUI, KC_LALT, KC_APP,  KC_BSPC, LT_CRSR, _______,          _______,    LT_CRSR, KC_ENT, _______,  _______,  _______,  LT_FUNC \
+    LT_MOUS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_CIRC,          JP_YEN,     KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_MINS, \
+    KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_LPRN,          JP_RPRN,    KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,  JP_COLN, \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    JP_LBRC,          JP_RBRC,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
+    LT_NUMB,  KC_LGUI, KC_LALT, KC_APP,  KC_BSPC, LT_CRSR, LT_FUNC,          LT_NUMB,    LT_CRSR, KC_ENT, _______,  JP_ZHTG,  _______,  LT_FUNC \
   ),
 
   /* LY_CURSOR
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   1  |   2  |   3  |   4  |   5  |   6  |  [   |   |  ]   |   7  |   8  |   9  |   0  |   -  |  ^   |
+   * |      |      |      |      |      |      |      |   |      |      | PgDn | PgUp | Home | PsCr | Bspc |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   * | Alt  |  F1  |      |Muhen | Henk |      |  (   |   |  )   | Left | Down |  Up  |Right |      |      |
+   * | Ctrl | HOME | Space| Del  |      |      |      |   | Home | Left | Down |  Up  |Right | End  |  '   |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Sft  |  F2  |  F3  |  F4  |  F5  |  F6  |  {   |   |  }   |  F7  |  F8  |  F9  | F10  | F11  |\/Sft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
@@ -58,16 +58,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [LY_CURSOR] = LAYOUT( \
-/*
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    _______,    _______, KC_7,    KC_8,    KC_9,  KC_0,    KC_MINS, KC_EQL, \
-    _______, KC_F1,   XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, _______,    _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX, XXXXXXX, \
-    _______, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______,    _______, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
-    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, RESET, _______, _______, _______ \
-*/
-    _______, _______, _______, KC_ENT,  _______, KC_TAB,  _______,    _______, _______, KC_PGDN, KC_PGUP,  KC_HOME,    KC_PSCR,   KC_BSPC, \
-    KC_LCTL, KC_HOME, KC_SPC,  KC_DEL,  _______, _______, _______,    KC_HOME, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,    KC_END,    KC_QUOT, \
-    KC_LSFT, _______, _______, _______, _______, KC_BSPC, _______,    _______, KC_ENT,  _______, _______,  _______,    KC_SLSH,   KC_RO,   \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), _______, _______,    _______, _______, S(KC_9), S(KC_0),  S(KC_MINS), S(KC_EQL), _______ \
+    _______, _______, _______, KC_END,  _______, KC_TAB,  _______,    _______, _______, KC_PGDN, KC_PGUP,  KC_HOME, KC_PSCR, KC_BSPC, \
+    KC_LCTL, KC_HOME, KC_SPC,  KC_DEL,  _______, _______, _______,    KC_HOME, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, KC_END,  KC_QUOT, \
+    KC_LSFT, _______, _______, _______, _______, KC_BSPC, _______,    _______, KC_ENT,  KC_ENT,  _______,  _______, KC_SLSH, KC_RO,   \
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,  _______, _______, _______ \
 ),
 
   /* LaYer MOUSE
@@ -84,8 +78,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LY_MOUSE] = LAYOUT( \
     _______, KC_BTN1, KC_BTN2, _______, KC_TAB,  XXXXXXX, _______,    _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, \
     KC_LCTL, KC_VOLU, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, _______,    _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, \
-    KC_LSFT, _______, _______, _______, _______, _______, _______,    _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______ \
+    KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______, \
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     _______, _______, _______, _______, _______, _______, _______ \
   ),
 
   /* LaYer NUMBER
@@ -103,12 +97,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, KC_ENT,  _______, KC_TAB,  _______,    _______,  _______, KC_7,    KC_8,    KC_9,    KC_MINS, KC_EQL,  \
     _______, _______, KC_SPC,  KC_DEL,  _______, _______, _______,    _______,  KC_BSPC, KC_4,    KC_5,    KC_6,    JP_PLUS, JP_ASTR, \
     _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,KC_BSPC, _______,    _______,  KC_0,    KC_1,    KC_2,    KC_3,    KC_SLSH, _______,\
-    _______, _______, _______, _______, _______, _______, _______,    _______,  _______,  _______, _______, _______, KC_DOT, _______  \
+    _______, _______, _______, _______, _______, _______, _______,    _______,  _______, _______, _______, _______, KC_DOT,  _______  \
   ),
 
   /* LaYer FUNCtion
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  @   |
+   * |  !   |   @  |   #  |   $  |   %  |   ^  |  &   |   |  *   |   Y  |   U  |   I  |   O  |   P  |  @   |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  :   |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
@@ -119,10 +113,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [LY_FUNC] = LAYOUT( \
-    S(KC_1),  S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6),  S(KC_7),   S(KC_8),    S(KC_9), S(KC_0), S(KC_MINS), S(KC_EQL),  S(KC_NUBS), S(KC_GRV), \
-    KC_LCTRL, _______, _______, _______, _______, _______, _______,    S(KC_BSLS), _______, _______, _______,    _______,    _______,    _______, \
-    KC_LSFT,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_GRV,     _______, _______, _______,    _______,    _______,    SFT_T(KC_RO), \
-    RESET,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     _______,    KC_ENT,  _______, _______,    _______,    KC_DOT,     _______  \
+ /*
+    JP_EXLM,  JP_AT,   JP_HASH, JP_DLR,  JP_PERC, JP_CIRC, JP_AMPR,   JP_ASTR,  JP_LPRN,   JP_RPRN,     JP_UNDS, JP_PLUS, JP_PIPE, JP_TILD, \
+    JP_EXLM,  JP_DQT,  JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT,   JP_LPRN,  JP_RPRN,   _______,     JP_EQL,  JP_TILD, JP_PIPE, KC_BSPC, \
+ */
+    JP_EXLM,  JP_DQT,  JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT,   JP_LPRN,  JP_RPRN,   _______,     JP_EQL,  JP_CIRC, JP_YEN,  KC_BSPC, \
+    _______,  _______, _______, _______, _______, _______, _______,   KC_GRV,   _______,SFT_T(KC_RO),   _______, JP_AT,   JP_LBRC, JP_RBRC, \
+    _______,  _______, _______, _______, _______, _______, _______,   _______,  _______,   _______,     JP_UNDS, JP_PLUS, JP_BSLS, KC_RSFT, \
+    RESET,    _______, _______, _______, _______, _______, _______,   _______,  _______,   _______,     _______, _______, _______, _______  \
   )
 
 };
