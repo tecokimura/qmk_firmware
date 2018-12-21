28d0379545c7b2b52ac8a239baee3e25ab8d2f9c
tecokimura <tecokimura@gmail.com>
2018-12-21 11:16:53 +0900

    mint60$B$r;H$&$?$a$N@_Dj(B;

--- keyboards/hhkb/keymaps/tecokimura/keymap.c
+++ keyboards/hhkb/keymaps/tecokimura/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_BSLS,     KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RO,   KC_UP,   KC_RSFT,
- LT_LFN, KC_ZKHK, KC_LGUI, KC_LALT, KC_BSPC,        LY_CRSR,        KC_ENT,  KC_KANA, KC_RALT, LT_RFN,    KC_LEFT, KC_DOWN, KC_RGHT),
+   LT_LFN, KC_ZKHK, KC_LGUI, KC_LALT, KC_BSPC,      LY_CRSR,        KC_ENT,  KC_KANA, KC_RALT, LT_RFN,   KC_LEFT, KC_DOWN, KC_RGHT),
 
     [LY_CRSR] = LAYOUT_JP(
         JP_ZHTG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_MINS, KC_EQL,  KC_JYEN, KC_BSPC,
