088a7ff95fef842f6946cbf24dda427b002a72f4
kimura <kimura@KIMURAK-M4.local>
2019-12-12 11:43:18 +0900

    fix Oyayubi keymap

--- keyboards/hhkb/keymaps/tecokimura/keymap.c
+++ keyboards/hhkb/keymaps/tecokimura/keymap.c
@@ -56,14 +56,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,     KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,   KC_UP,   KC_RSFT,
-   LT_LFN, KC_ZKHK, KC_LGUI, KC_LALT, KC_BSPC,      LT_CRSR,        KC_ENT,  KC_KANA, KC_RALT, LT_RFN,   KC_LEFT, KC_DOWN, KC_RGHT),
+   LT_LFN, KC_LALT, KC_LGUI, KC_BSPC, KC_BSPC,      LT_CRSR,        KC_ENT,  KC_ENT,  KC_RALT,  LT_RFN,  KC_LEFT, KC_DOWN,   KC_RGHT),
 
     [LY_CRSR] = LAYOUT_JP(
         JP_ZHTG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_MINS, KC_EQL,  KC_JYEN, KC_BSPC,
         _______, _______, _______, KC_END,  _______, _______, KC_HOME, KC_PGDN, KC_PGUP, _______, KC_PSCR, KC_LBRC, KC_RBRC,
         KC_LCTL, KC_HOME, KC_SPC,  KC_DEL,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,  KC_QUOT, KC_BSLS, KC_ENT,
         KC_LSFT, _______, _______, _______, _______, KC_BSPC, KC_ENT,  _______, KC_COMM, KC_DOT,  KC_SLSH, KC_RO,   KC_UP,   KC_RSFT,
- _______, _______, _______, KC_LALT, _______,       _______,       _______, _______, _______,  _______,    KC_LEFT, KC_DOWN, KC_RGHT),
+  _______, KC_ZKHK, _______, KC_LALT, _______,       _______,       _______, KC_KANA, _______,  _______,    KC_LEFT, KC_DOWN, KC_RGHT),
 
     [LY_MOUS] = LAYOUT_JP(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_JYEN, KC_BSPC,
